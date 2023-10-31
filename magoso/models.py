4bfca941f6ccc9b8ead4934bc6816b76b65211e2
JamesMusyoka <jamesmu475@gmail.com>
2023-10-31 12:33:32 +0300

    Accomplished some codes

--- magoso/models.py
+++ magoso/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.as
 class user(models.Model):
     user_id = models.CharField(max_length=50, unique=True)
     name = models.OneToOneField(User, on_delete=models.CASCADE)

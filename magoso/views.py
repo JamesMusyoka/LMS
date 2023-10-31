2e48a5514fdf432956d73225bdd8d8e6108f3a93
JamesMusyoka <jamesmu475@gmail.com>
2023-10-31 11:44:54 +0300

    Added some codes for our site

--- magoso/views.py
+++ magoso/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello Magoso Tech Academy')
+    return HttpResponse('Welcome to Magoso Tech Academy!')

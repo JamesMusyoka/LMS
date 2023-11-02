335cf1da29f4f5e496bac426d806ad1c46e33ab0
JamesMusyoka <jamesmu475@gmail.com>
2023-11-02 11:07:39 +0300

    Imported os to the models file

--- magoso/models.py
+++ magoso/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+
 
 class UserProfile(models.Model):
     # user_id = models.CharField(max_length=50, unique=True)
